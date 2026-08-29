3e2d6734b39175d9cc7b3e8475b9c4295f579eb2
John Sterlacci <johntsterlacci@gmail.com>
2026-08-29 18:15:23 -0400

    Strengthen run ID uniqueness

--- framework/rng.js
+++ framework/rng.js
@@ -1,5 +1,5 @@
 import { randomUUID } from "node:crypto";
 
 export function createRunId() {
-  return `${new Date().toISOString().replaceAll(/[-:.TZ]/g, "")}-${randomUUID().slice(0, 8)}`;
+  return `${new Date().toISOString().replaceAll(/[-:.TZ]/g, "")}-${randomUUID()}`;
 }
